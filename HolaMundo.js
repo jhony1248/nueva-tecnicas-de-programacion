2de67424f63a8c6c32552d4c672b27f363d83804
Jhony Hurtado <60785005+jhony1248@users.noreply.github.com>
2021-06-21 16:42:47 -0500

    Delete HolaMundo.js

--- HolaMundo.js
+++
@@ -1,6 +0,0 @@
-console.log("Hola mundo");
-console.warn("Este es un mensaje de aviso");
-Console._exception("Error")
-console.table(["apples", "oranges", "bananas"]);
-console.dir(objeto);
-Console.count()
